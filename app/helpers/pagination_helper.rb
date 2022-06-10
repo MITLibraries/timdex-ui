e96d1247d928e90f489a17719c758494ae89697a
Matt Bernhardt <mjbernha@mit.edu>
2022-06-10 17:00:48 -0400

    Improvements to pagination UI on results page
    ** Why are these changes being introduced:
    
    * There are some UI improvements for the pagination panel called for as
      part of RDI-100
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/rdi-143
    
    ** How does this address that need:
    
    * This changes the presentation of the pagination area. No behavioral
      changes are part of this change.
    * Some tests are updated because the resulting tag structure for the
      pagination controls has changed. I'm not sure how to avoid this sort
      of test fragility.
    
    ** Document any side effects to this change:
    
    * Some tests change because we're testing for specific markup - which
      now feels fragile to me, but I'm not sure how to improve it.
    * We make more use of font-awesome icons now because of the arros in
      the next/prev buttons. This feels acceptable to me, at least until
      UX can weigh in.

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -1,11 +1,15 @@
 module PaginationHelper
   def next_url(query_params)
     query_params[:page] = @pagination[:next]
-    link_to('Next page', results_path(query_params))
+    link_to results_path(query_params), class: 'btn button-primary' do
+      "Next page #{content_tag(:span, '', class: 'fa fa-chevron-right')}".html_safe
+    end
   end
 
   def prev_url(query_params)
     query_params[:page] = @pagination[:prev]
-    link_to('Previous page', results_path(query_params))
+    link_to results_path(query_params), class: 'btn button-primary' do
+      "#{content_tag(:span, '', class: 'fa fa-chevron-left')} Previous page".html_safe
+    end
   end
 end
