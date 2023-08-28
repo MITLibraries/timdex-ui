69ce1bc0804638f815be425ce57b0324300203d4
Matthew Bernhardt <mjbernha@mit.edu>
2023-08-28 15:20:06 -0400

    Implement a first experiment
    This sets up a simple UI experiment, defining an alternative set of styles for the search form.
    
    The search form starts in the _form.html.erb partial, with most users getting a "standard" class - the experimental group would get the "alt" class, which is rendered differently.
    
    The experiment ends when a user reaches a search results page (i.e, they submitted the form and thus got results).
    
    Curious folks can force themselves into one or the other pool by using a querystring such as
    
    ?ab_test[ui_colors]=alt
    or
    ?ab_test[ui_colors]=standard
    
    Refactor UI experiment based on recent changes
    
    Fixup search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,6 +12,9 @@ class SearchController < ApplicationController
   before_action :validate_geodistance_units!, only: %i[results]
 
   def results
+    # if we are loading results, the user submitted the form - so this experiment is finished
+    ab_finished(:ui_colors)
+
     # inject session preference for boolean type if it is present
     params[:booleanType] = cookies[:boolean_type] || 'AND'
 
