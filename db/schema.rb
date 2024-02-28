02d4a794734d2d322ac23a96539e1852fad7f04d
Jeremy Prevost <JPrevost@users.noreply.github.com>
2024-02-28 15:27:39 -0500

    Rails app:update: missing migrations
    We don't use ActiveStorage in this app, but app:update detected these
    migrations were missing so I feel it's better to add them for now.
    
    We can more fully remove ActiveStorage from this app in the future, but
    I was not prepared to address that during this maintenance cycle.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_30_222044) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_201656) do
   create_table "flipflop_features", force: :cascade do |t|
     t.string "key", null: false
     t.boolean "enabled", default: false, null: false
