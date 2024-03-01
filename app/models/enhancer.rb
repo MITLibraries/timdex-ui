ab8f1eb1058cf67898d6cabeb75e78085c4d7fd7
jazairi <16103405+jazairi@users.noreply.github.com>
2024-03-01 14:40:19 -0500

    Implement location-based search
    Why these changes are being introduced:
    
    The GeoData app needs to allow for geospatial searching.
    
    Relevant ticket(s):
    
    * [GDT-136](https://mitlibraries.atlassian.net/browse/GDT-136)
    
    How this addresses that need:
    
    This adds form elements for the geospatial fields we’ve added to TIMDEX: geobox and geodistance. Because these fields
    are relatively complex and all their arguments are non-nullable, each of them has its own drop-down that toggles a
    boolean param (`:geobox` and `:geodistance`, respectively) to pass the field’s arguments into the TIMDEX query.
    
    Certain client-side form validations have been added (e.g., range for lat/long entries), as well as corresponding
    controller validations in case a user bypasses the form.
    
    In order to achieve the multiple query permutations (no geo fields, both geo fields, one of either geo field),
    additional query objects have been added. This is based on the recommendation of the graphql-client gem, which
    suggests using statically declared heredocs as inputs. However, that gem also recommends making queries out of several
    smaller heredocs, using GraphQL conveniences like fragments, so our implementation may not be exactly what the gem
    maintainers intended.
    
    Side effects of this change:
    
    * Introducing an additional monolithic query object for each permutation doesn’t feel scalable or OO. We likely don’t
    have to time to investigate this now, but it would be interesting to consider alternative methods to constructing
    queries, and possibly also different GraphQL client gems.
    * The controller validations are an important failsafe, but they are not especially friendly. We may want to consider
    additional client-side validations to reduce the likelihood of encountering the controller validations in the wild.
    * Writing this feature revealed that the Enhancer and Query Builder models are functionally identical. This was
    intentional to prepare the app for additional query enhancements. As we’ve now abstracted that to TACOS, we should
    consider whether we still need both of these models.
    * The form partial and corresponding JS are getting unwieldy. Refactoring these would be a good maintenance activity.
    Further, the interactive elements are becoming complex enough that system testing is worth considering.
    * I included sample values in placeholder text to avoid making the descriptions any longer than necessary. However,
    this may cause an unequal user experience, as some assistive technology skips placeholders. I plan to ask about this
    during UX review.
    * There are a lot of geo tests, so rather than switching the test strategy in each test, I isolated them to their
    own class to set the test strategy in a minitest lifecycle hook.
    * Most cassettes have been regenerated due to schema updates.

--- app/models/enhancer.rb
+++ app/models/enhancer.rb
@@ -4,14 +4,22 @@ class Enhancer
   QUERY_PARAMS = %i[q citation contentType contributors fundingInformation identifiers locations subjects title].freeze
   FILTER_PARAMS = %i[contentTypeFilter contributorsFilter formatFilter languagesFilter literaryFormFilter placesFilter
                      sourceFilter subjectsFilter].freeze
+  GEO_PARAMS = %i[geoboxMinLongitude geoboxMinLatitude geoboxMaxLongitude geoboxMaxLatitude geodistanceLatitude
+                  geodistanceLongitude geodistanceDistance].freeze
 
   # accepts all params as each enhancer may require different data
   def initialize(params)
     @enhanced_query = {}
     @enhanced_query[:page] = calculate_page(params[:page].to_i)
-    @enhanced_query[:advanced] = true if params[:advanced].present?
+    @enhanced_query[:advanced] = 'true' if params[:advanced].present?
+
+    if Flipflop.enabled?(:gdt)
+      @enhanced_query[:geobox] = 'true' if params[:geobox] == 'true'
+      @enhanced_query[:geodistance] = 'true' if params[:geodistance] == 'true'
+    end
 
     extract_query(params)
+    extract_geosearch(params)
     extract_filters(params)
     patterns(params) if params[:q]
   end
@@ -25,6 +33,14 @@ class Enhancer
   def extract_query(params)
     QUERY_PARAMS.each do |qp|
       @enhanced_query[qp] = params[qp] if params[qp].present?
+    end
+  end
+
+  def extract_geosearch(params)
+    return unless Flipflop.enabled?(:gdt)
+
+    GEO_PARAMS.each do |gp|
+      @enhanced_query[gp] = params[gp] if params[gp].present?
     end
   end
 
