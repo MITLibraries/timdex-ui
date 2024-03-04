ce9068801c8486b242de35668f5e529148952deb
Matthew Bernhardt <mjbernha@mit.edu>
2024-03-04 15:36:37 -0500

    Address code review feedback
    Adam rightly pointed out that the mockups in this ticket called for the
    mobile filter behavior to kick in at the medium screen size (below 768),
    not small (below 480).
    
    This swaps the change over to that screen size, replacing -sm with -md
    as needed.

--- app/javascript/filters.js
+++ app/javascript/filters.js
@@ -3,7 +3,7 @@ function initFilterToggle() {
   var filter_toggle = document.getElementById('filter-toggle');
   var filter_panel = document.getElementById('filters');
   filter_toggle.addEventListener('click', event => {
-    filter_panel.classList.toggle('hidden-sm');
+    filter_panel.classList.toggle('hidden-md');
     filter_toggle.classList.toggle('expanded');
 
   });
