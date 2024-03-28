b458cd4c9efd7a14633b80605f39e9f47366d2b7
jazairi <16103405+jazairi@users.noreply.github.com>
2024-03-28 13:36:06 -0400

    Move results context from filter sidebar to results container
    Why these changes are being introduced:
    
    We will be moving the filter sidebar further down the tab order,
    so screen reader users would not reach the number of search results
    until they reach the end of the results. This foregrounds that
    context, both for blind/low-vision and sighted users.
    
    Relevant ticket(s):
    
    * [GDT-235](https://mitlibraries.atlassian.net/browse/GDT-235)
    
    How this addresses that need:
    
    This moves the number of results returned to the top of the
    results container, making it the first focusable element when
    a screen reader user skips to results.
    
    Side effects of this change:
    
    The results ID moves up one level in the DOM to account for the
    new heading.

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,5 +1,5 @@
 module ResultsHelper
   def results_summary(hits)
-    hits.to_i >= 10_000 ? '10,000+ items' : "#{number_with_delimiter(hits)} items"
+    hits.to_i >= 10_000 ? '10,000+ results' : "#{number_with_delimiter(hits)} results"
   end
 end
