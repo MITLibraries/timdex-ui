b1308e5c35ad56b958d1feac5cf71fa9dee39ba0
Jeremy Prevost <JPrevost@users.noreply.github.com>
2025-10-15 08:03:35 -0400

    Add TACOS suggestions
    Why are these changes being introduced:
    
    * Displaying panels with TACOS suggestions on the search results
      and pages when they are available will help users access the resources
      they need more quickly.
    
    Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/USE-65
    
    How does this address that need:
    
    * Brings in the draft TACOS styles and views that Dave build in TACOS
      and modifies them to fit into the existing application styles and
      views.
    * Extracts the suggestions from the tacos response and displays them
      in a panel on the search results and analyze pages.
    
    Document any side effects to this change:
    
    * Only added to non-GDT searches at this time.
    
    Co-Authored-By: djanelle-mit <181018533+djanelle-mit@users.noreply.github.com>

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -4,6 +4,10 @@ class TacosController < ApplicationController
   def analyze
     return unless ApplicationHelper.tacos_enabled?
 
-    Tacos.analyze(params[:q])
+    tacos_response = Tacos.analyze(params[:q])
+
+    # Suggestions return as an array but we don't want to display more than one.
+    # We may want to have a "priority" system in the future to determine which suggestion to show.
+    @suggestions = tacos_response['data']['logSearchEvent']['detectors']['suggestedResources'].first
   end
 end
