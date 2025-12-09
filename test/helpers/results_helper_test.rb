3f74b5e66ba76d9dc641805a5f992cb899766e3b
Jeremy Prevost <JPrevost@users.noreply.github.com>
2025-12-09 16:13:06 -0500

    Add tests for search url helpers and icon

--- test/helpers/results_helper_test.rb
+++ test/helpers/results_helper_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class ResultsHelperTest < ActionView::TestCase
   include ResultsHelper
 
+  require 'uri'
+
   test 'if number of hits is equal to 10,000, results summary returns "10,000+"' do
     hits = 10_000
     assert_equal '10,000+ results', results_summary(hits)
@@ -50,5 +52,39 @@ class ResultsHelperTest < ActionView::TestCase
     params[:tab] = 'website'
     description = 'Information about the library: events, news, services, and more'
     assert_equal description, tab_description
+  end
+
+  test 'search_primo_params includes encoded search query' do
+    params[:q] = 'breakfast of champions'
+    result = search_primo_params
+
+    assert_includes result, 'query=any%2Ccontains%2Cbreakfast+of+champions'
+    assert_includes result, 'tab=all'
+    assert_includes result, 'search_scope=all'
+  end
+
+  test 'search_primo_link returns a valid URL string' do
+    params[:q] = 'test query'
+    link = search_primo_link
+
+    assert link.start_with?('https://')
+    assert_includes link, '/discovery/search?'
+    assert_includes link, 'query=any%2Ccontains%2Ctest+query'
+  end
+
+  test 'search_aspace_link returns a valid archivesspace search URL' do
+    params[:q] = 'minutes'
+    link = search_aspace_link
+
+    assert link.start_with?('https://archivesspace.mit.edu/search?')
+    assert_includes link, 'q%5B%5D=minutes'
+  end
+
+  test 'search_worldcat_link returns a valid worldcat search URL' do
+    params[:q] = 'climate change'
+    link = search_worldcat_link
+
+    assert link.start_with?('https://mit.on.worldcat.org/search?')
+    assert_includes link, 'queryString=climate+change'
   end
 end
