c54525aaee655dee217e68a1d238f2735aa2b824
jazairi <16103405+jazairi@users.noreply.github.com>
2026-07-22 16:29:45 -0700

    Rails app:update 8.0 generated files

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative "boot"
 require "rails/all"
 
 require 'rack/attack'
-require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,10 +12,6 @@ module TimdexUi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-
-    # Enable asset pipeline
-    config.assets.enabled = true
-    config.assets.version = '1.0'
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
