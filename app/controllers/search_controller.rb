439fd2d715aae96d96fa38da0b513cf8e0e39a33
Matthew Bernhardt <mjbernha@mit.edu>
2026-08-27 13:05:56 -0400

    Fix for behavior

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -45,13 +45,20 @@ class SearchController < ApplicationController
 
     # Render the response in HTML or JSON format
     respond_to do |format|
-      format.json { render json: { results: @results, pagination: @pagination, errors: @errors } }
+      format.json { render json: { results: @results, pagination: hit_count, errors: @errors } }
       format.turbo_stream { render :results }
       format.html { render :results }
     end
   end
 
   private
+
+  # This returns the total result count from whichever instance variable is present
+  def hit_count
+    return @pagination unless pagination_load_more_enabled?
+
+    { hits: @load_more[:total_hits] }
+  end
 
   # Sleep to simulate latency for testing loading indicators when responses are fast
   def sleep_if_too_fast
